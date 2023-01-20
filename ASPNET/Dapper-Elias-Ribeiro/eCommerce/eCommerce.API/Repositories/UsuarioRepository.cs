99fe7496a05b5787fa4d89444b800b1afdb9824d
Danilo Silva <danilo.silva@msn.com>
2023-01-20 18:11:05 -0300

    S0504 - Pegar Usuarios

--- ASPNET/Dapper-Elias-Ribeiro/eCommerce/eCommerce.API/Repositories/UsuarioRepository.cs
+++ ASPNET/Dapper-Elias-Ribeiro/eCommerce/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using Dapper;
 using eCommerce.API.Models;
 using Microsoft.Data.SqlClient;
 
@@ -15,15 +16,9 @@ namespace eCommerce.API.Repositories
             _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
-        private static List<Usuario> _db = new List<Usuario>()
-        {
-            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
-            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
-            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
-        };
         public List<Usuario> Get()
         {
-            return _db;
+            return _connection.Query<Usuario>("SELECT * FROM Usuarios").ToList();
         }
 
         public Usuario Get(int id)
@@ -57,5 +52,12 @@ namespace eCommerce.API.Repositories
         {
             _db.Remove(_db.FirstOrDefault(a => a.Id == id));
         }
+
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
+            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
+            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
+        };
     }
 }
