e01be5798b2bc44224ea210ca9938aeed25f653d
Danilo Silva <danilo.silva@msn.com>
2023-01-21 09:14:47 -0300

    S09E02 - ContribUsuarioRepository Completo

--- ASPNET/Dapper-Elias-Ribeiro/eCommerce/eCommerce.API/Repositories/ContribUsuarioRepository.cs
+++ ASPNET/Dapper-Elias-Ribeiro/eCommerce/eCommerce.API/Repositories/ContribUsuarioRepository.cs
@@ -1,34 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper.Contrib.Extensions;
 using eCommerce.API.Models;
+using Microsoft.Data.SqlClient;
 
 namespace eCommerce.API.Repositories
 {
     public class ContribUsuarioRepository : IUsuarioRepository
     {
+        private IDbConnection _connection;
+
+        public ContribUsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
         public List<Usuario> Get()
         {
-            throw new NotImplementedException();
+            return _connection.GetAll<Usuario>().ToList();
         }
 
         public Usuario Get(int id)
         {
-            throw new NotImplementedException();
+            return _connection.Get<Usuario>(id);
         }
 
         public void Insert(Usuario usuario)
         {
-            throw new NotImplementedException();
+            usuario.Id = Convert.ToInt32(_connection.Insert(usuario));
         }
 
         public void Update(Usuario usuario)
         {
-            throw new NotImplementedException();
+            _connection.Update(usuario);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            _connection.Delete(Get(id));
         }
     }
 }
