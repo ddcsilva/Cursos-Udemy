e3c6c2dd12ecf1337b9c66ebfa30de53dea8296e
Danilo Silva <danilo.silva@msn.com>
2023-01-21 09:08:07 -0300

    S09E01 - Estruturando o projeto para o Dapper.Contrib

---
+++ ASPNET/Dapper-Elias-Ribeiro/eCommerce/eCommerce.API/Repositories/ContribUsuarioRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class ContribUsuarioRepository : IUsuarioRepository
+    {
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
