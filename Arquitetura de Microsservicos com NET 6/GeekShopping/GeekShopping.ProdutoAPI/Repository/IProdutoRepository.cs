7f84bd934bab4ecda957debe68409b87d321733a
danilo.silva <danilo.silva@msn.com>
2024-02-12 18:08:15 -0300

    Definindo as Propriedades da Interface IProductRepository

--- Arquitetura de Microsservicos com NET 6/GeekShopping/GeekShopping.ProdutoAPI/Repository/IProdutoRepository.cs
+++ Arquitetura de Microsservicos com NET 6/GeekShopping/GeekShopping.ProdutoAPI/Repository/IProdutoRepository.cs
@@ -1,5 +1,12 @@
+using GeekShopping.ProdutoAPI.Data.ValueObjects;
+
 namespace GeekShopping.ProdutoAPI.Repository;
 
 public interface IProdutoRepository
 {
+    Task<IEnumerable<ProdutoVO>> BuscarTodos();
+    Task<ProdutoVO> BuscarPorId(long id);
+    Task<ProdutoVO> Inserir(ProdutoVO produto);
+    Task<ProdutoVO> Atualizar(ProdutoVO produto);
+    Task<bool> Excluir(long id);
 }
