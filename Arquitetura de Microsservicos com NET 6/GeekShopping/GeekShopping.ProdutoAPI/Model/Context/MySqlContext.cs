11540d459a3d06b2d3bea25ef503366a3d2b1662
danilo.silva <danilo.silva@msn.com>
2024-02-12 20:50:48 -0300

    Configurando o Seed Data de Produtos

--- Arquitetura de Microsservicos com NET 6/GeekShopping/GeekShopping.ProdutoAPI/Model/Context/MySqlContext.cs
+++ Arquitetura de Microsservicos com NET 6/GeekShopping/GeekShopping.ProdutoAPI/Model/Context/MySqlContext.cs
@@ -11,4 +11,128 @@ public class MySqlContext : DbContext
     public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
 
     public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 1,
+            Nome = "Caneca Geek",
+            Preco = new decimal(49.9),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/1_super_mario.jpg?raw=true",
+            NomeCategoria = "Caneca"
+        });
+
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 2,
+            Nome = "Camiseta No Internet",
+            Preco = new decimal(69.9),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/2_no_internet.jpg?raw=true",
+            NomeCategoria = "T-shirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 3,
+            Nome = "Capacete Darth Vader Star Wars Black Series",
+            Preco = new decimal(999.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/3_vader.jpg?raw=true",
+            NomeCategoria = "Action Figure"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 4,
+            Nome = "Star Wars The Black Series Hasbro - Stormtrooper Imperial",
+            Preco = new decimal(189.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/4_storm_tropper.jpg?raw=true",
+            NomeCategoria = "Action Figure"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 5,
+            Nome = "Camiseta Gamer",
+            Preco = new decimal(69.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/5_100_gamer.jpg?raw=true",
+            NomeCategoria = "T-shirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 6,
+            Nome = "Camiseta SpaceX",
+            Preco = new decimal(49.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/6_spacex.jpg?raw=true",
+            NomeCategoria = "T-shirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 7,
+            Nome = "Camiseta Feminina Coffee Benefits",
+            Preco = new decimal(69.9),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/7_coffee.jpg?raw=true",
+            NomeCategoria = "T-shirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 8,
+            Nome = "Moletom Com Capuz Cobra Kai",
+            Preco = new decimal(159.9),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/8_moletom_cobra_kay.jpg?raw=true",
+            NomeCategoria = "Sweatshirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 9,
+            Nome = "Livro Star Talk – Neil DeGrasse Tyson",
+            Preco = new decimal(49.9),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/9_neil.jpg?raw=true",
+            NomeCategoria = "Book"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 10,
+            Nome = "Star Wars Mission Fleet Han Solo Nave Milennium Falcon",
+            Preco = new decimal(359.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true",
+            NomeCategoria = "Action Figure"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 11,
+            Nome = "Camiseta Elon Musk Spacex Marte Occupy Mars",
+            Preco = new decimal(59.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/11_mars.jpg?raw=true",
+            NomeCategoria = "T-shirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 12,
+            Nome = "Camiseta GNU Linux Programador Masculina",
+            Preco = new decimal(59.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/12_gnu_linux.jpg?raw=true",
+            NomeCategoria = "T-shirt"
+        });
+        modelBuilder.Entity<Produto>().HasData(new Produto
+        {
+            Id = 13,
+            Nome = "Camiseta Goku Fases",
+            Preco = new decimal(59.99),
+            Descricao = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+            ImagemUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/13_dragon_ball.jpg",
+            NomeCategoria = "T-shirt"
+        });
+    }
 }
