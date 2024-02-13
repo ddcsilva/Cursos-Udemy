34e96d7946ddf4d5e2172ad992e42ac31207b82e
danilo.silva <danilo.silva@msn.com>
2024-02-13 13:27:55 -0300

    Implementando o Controller de Produtos

---
+++ Arquitetura de Microsservicos com NET 6/GeekShopping/GeekShopping.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,20 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers;
+
+public class ProdutosController : Controller
+{
+    private readonly IProdutoService _produtoService;
+
+    public ProdutosController(IProdutoService produtoService)
+    {
+        _produtoService = produtoService ?? throw new ArgumentNullException(nameof(produtoService));
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var produtos = await _produtoService.ObterTodosProdutos();
+        return View(produtos);
+    }
+}
