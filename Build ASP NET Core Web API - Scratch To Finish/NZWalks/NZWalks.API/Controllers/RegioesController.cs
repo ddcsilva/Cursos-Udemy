55a4be5fde1abba6d17946c3c728d70cc37ddb28
danilo.silva <danilo.silva@msn.com>
2024-02-21 18:36:26 -0300

    Action -> Atualizar Região

--- Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Controllers/RegioesController.cs
+++ Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Controllers/RegioesController.cs
@@ -26,6 +26,24 @@ public class RegioesController : ControllerBase
     [HttpGet]
     public IActionResult ObterTodos()
     {
+        /*
+            Mocks para exemplo:
+            Id: 1
+            Codigo: NOR
+            Nome: Northland
+            ImagemUrl: https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/northland/northland-landscape.jpg
+
+            Id: 2
+            Codigo: AUC
+            Nome: Auckland
+            ImagemUrl: https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/auckland/auckland-landscape.jpg
+
+            Id: 3
+            Codigo: WAI
+            Nome: Waikato
+            ImagemUrl: https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/waikato/waikato-landscape.jpg
+        */
+
         // Obtendo dados do banco de dados. Domain Model.
         // ToList() é um método de extensão que executa a query no banco de dados e retorna uma lista.
         var regioes = _context.Regioes.ToList();
@@ -78,6 +96,11 @@ public class RegioesController : ControllerBase
         return Ok(regiaoDTO);
     }
 
+    /// <summary>
+    /// Action para cadastrar uma região.
+    /// </summary>
+    /// <param name="request"> DTO com os dados da região. </param>
+    /// <returns> Região cadastrada. </returns>
     [HttpPost]
     public IActionResult Cadastrar([FromBody] CadastrarRegiaoRequestDTO request)
     {
@@ -105,5 +128,40 @@ public class RegioesController : ControllerBase
         // CreatedAtAction() é um método que retorna um status 201.
         // Ele também retorna um cabeçalho Location com a URL para obter a entidade criada.
         return CreatedAtAction(nameof(ObterPorId), new { id = regiao.Id }, regiao);
+    }
+
+    [HttpPut]
+    [Route("{id:Guid}")]
+    public IActionResult Atualizar([FromRoute] Guid id, [FromBody] AtualizarRegiaoRequestDTO request)
+    {
+        // Obtendo dados do banco de dados. Domain Model.
+        // FirstOrDefault() é um método do Entity Framework que busca uma entidade com base em um predicado.
+        var regiao = _context.Regioes.FirstOrDefault(r => r.Id == id);
+
+        if (regiao == null)
+        {
+            return NotFound();
+        }
+
+        // Mapendo os dados do DTO para o Domain Model.
+        regiao.Codigo = request.Codigo;
+        regiao.Nome = request.Nome;
+        regiao.ImagemUrl = request.ImagemUrl;
+
+        // Atualizando a entidade no contexto.
+        // Não é necessário chamar o método Update() do DbSet, pois o Entity Framework rastreia as entidades.
+        _context.SaveChanges();
+
+        // Mapeando os dados para um DTO (Data Transfer Object).
+        var regiaoDTO = new RegiaoDTO
+        {
+            Id = regiao.Id,
+            Codigo = regiao.Codigo,
+            Nome = regiao.Nome,
+            ImagemUrl = regiao.ImagemUrl
+        };
+
+        // Ok() é um método que retorna um status 200.
+        return Ok(regiaoDTO);
     }
 }
---
+++ Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Models/DTO/AtualizarRegiaoRequestDTO.cs
@@ -0,0 +1,8 @@
+namespace NZWalks.API.Models.DTO;
+
+public class AtualizarRegiaoRequestDTO
+{
+    public string Codigo { get; set; }
+    public string Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
