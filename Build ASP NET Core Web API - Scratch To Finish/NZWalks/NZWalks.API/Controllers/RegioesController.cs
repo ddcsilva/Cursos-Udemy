4cc0b3056848b6d133c52c050448b8d6f41c4d06
danilo.silva <danilo.silva@msn.com>
2024-02-21 15:42:04 -0300

    Action -> Obter Regiões

---
+++ Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Controllers/RegioesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RegioesController : ControllerBase
+{
+    private readonly NZWalksDbContext _context;
+
+    public RegioesController(NZWalksDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult ObterTodos()
+    {
+        var regioes = _context.Regioes.ToList();
+
+        return Ok(regioes);
+    }
+}
