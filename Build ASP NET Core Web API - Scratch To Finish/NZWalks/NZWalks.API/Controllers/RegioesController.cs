001e17f4512967a148b2ad3c33f28c50eb6ccde4
danilo.silva <danilo.silva@msn.com>
2024-02-21 15:52:44 -0300

    Utilizando DTO

--- Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Controllers/RegioesController.cs
+++ Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Controllers/RegioesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers;
 
@@ -24,8 +25,22 @@ public class RegioesController : ControllerBase
     [HttpGet]
     public IActionResult ObterTodos()
     {
+        // Obtendo dados do banco de dados. Domain Model.
         // ToList() é um método de extensão que executa a query no banco de dados e retorna uma lista.
         var regioes = _context.Regioes.ToList();
+
+        // Mapeando os dados para um DTO (Data Transfer Object).
+        var regioesDto = new List<RegiaoDTO>();
+        foreach (var regiaoDomain in regioes)
+        {
+            regioesDto.Add(new RegiaoDTO
+            {
+                Id = regiaoDomain.Id,
+                Codigo = regiaoDomain.Codigo,
+                Nome = regiaoDomain.Nome,
+                ImagemUrl = regiaoDomain.ImagemUrl
+            });
+        }
 
         // Ok() é um método que retorna um status 200.
         return Ok(regioes);
@@ -35,6 +50,7 @@ public class RegioesController : ControllerBase
     [Route("{id:Guid}")]
     public IActionResult ObterPorId([FromRoute] Guid id) // FromRoute é um atributo que indica que o parâmetro vem da rota.
     {
+        // Obtendo dados do banco de dados. Domain Model.
         // Find() é um método do Entity Framework que busca uma entidade pelo seu ID.
         var regiao = _context.Regioes.Find(id);
 
@@ -44,6 +60,15 @@ public class RegioesController : ControllerBase
             return NotFound();
         }
 
-        return Ok(regiao);
+        // Mapeando os dados para um DTO (Data Transfer Object).
+        var regiaoDto = new RegiaoDTO
+        {
+            Id = regiao.Id,
+            Codigo = regiao.Codigo,
+            Nome = regiao.Nome,
+            ImagemUrl = regiao.ImagemUrl
+        };
+
+        return Ok(regiaoDto);
     }
 }
---
+++ Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Models/DTO/RegiaoDTO.cs
@@ -0,0 +1,9 @@
+namespace NZWalks.API.Models.DTO;
+
+public class RegiaoDTO
+{
+    public Guid Id { get; set; }
+    public string Codigo { get; set; }
+    public string Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
