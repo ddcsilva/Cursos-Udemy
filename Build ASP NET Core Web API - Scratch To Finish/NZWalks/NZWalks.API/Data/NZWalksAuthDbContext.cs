fb94c0a1807e037ed3d4390e7b5f6ccb39408a84
danilo.silva <danilo.silva@msn.com>
2024-02-26 23:33:02 -0300

    Aplicando Seed

--- Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ Build ASP NET Core Web API - Scratch To Finish/NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data;
@@ -8,4 +9,32 @@ namespace NZWalks.API.Data;
 public class NZWalksAuthDbContext : IdentityDbContext
 {
     public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var readerRoleId = "80eba5e1-33ee-4444-81b2-4edee2f25f99";
+        var writerRoleId = "d789895d-07c0-468c-9d8c-07e110937e30";
+
+        var roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Id = readerRoleId,
+                ConcurrencyStamp = readerRoleId,
+                Name = "Reader",
+                NormalizedName = "READER"
+            },
+            new IdentityRole
+            {
+                Id = writerRoleId,
+                ConcurrencyStamp = writerRoleId,
+                Name = "Writer",
+                NormalizedName = "WRITER"
+            }
+        };
+
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
 }
