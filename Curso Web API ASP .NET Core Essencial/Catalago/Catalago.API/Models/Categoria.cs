6cbf94c1c81cfa3bcdb990457f31ac6b0295cebb
danilo.silva <danilo.silva@msn.com>
2024-02-28 18:56:56 -0300

    Criando o modelo de domínio

---
+++ Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Models/Categoria.cs
@@ -0,0 +1,8 @@
+namespace Catalago.Api.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
