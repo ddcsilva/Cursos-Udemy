f3126001c71ab7ee3a1e52d7ac6c5eabb678ffa9
danilo.silva <danilo.silva@msn.com>
2024-02-28 19:08:56 -0300

    Registrando os serviços no container DI

--- Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Program.cs
+++ Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Program.cs
@@ -1,8 +1,17 @@
+using Catalago.Api.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
 
 var app = builder.Build();
 
