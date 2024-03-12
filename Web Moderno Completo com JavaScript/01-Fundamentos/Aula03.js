1eb4b74d692af365d2a625467fbda9dfbb4d1185
danilo.silva <danilo.silva@msn.com>
2024-03-12 20:31:47 -0300

    Sentenças e Blocos

---
+++ Web Moderno Completo com JavaScript/01-Fundamentos/Aula03.js
@@ -0,0 +1,15 @@
+/*
+    Bloco de código => conjunto de sentenças de código. Utiliza chaves para delimitar o bloco
+    Sentença de código => conjunto de palavras que executam uma ação. Termina com ponto e vírgula.
+*/
+
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
