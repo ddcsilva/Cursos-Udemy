49e9abc0cf93e7f106673c180413f8ab1524f424
danilo.silva <danilo.silva@msn.com>
2024-03-12 20:48:02 -0300

    Trabalhando com Dados 03

---
+++ Web Moderno Completo com JavaScript/01-Introducao/aula06.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof 31); // number
+console.log(typeof idade); // number
+
+let salario = 4578.32;
+console.log(typeof salario); // number
+
+let chovendo = true; // ou false
+console.log(typeof chovendo); // boolean
+
+console.log(typeof "Teste"); // string
