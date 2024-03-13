2d27fe8dd5e33e7def8334cead15af0430609d4f
danilo.silva <danilo.silva@msn.com>
2024-03-13 18:38:34 -0300

    Context e Connection

--- The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Models/Villa.cs
+++ The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Models/Villa.cs
@@ -1,7 +1,12 @@
-﻿namespace MagicVilla.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla.API.Models;
 
 public class Vila
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Detalhes { get; set; }
