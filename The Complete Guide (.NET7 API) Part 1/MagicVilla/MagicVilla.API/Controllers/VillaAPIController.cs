d94cf12278d62bee81f04b6dbad4299948102273
danilo.silva <danilo.silva@msn.com>
2024-03-13 08:36:07 -0300

    Primeiro Endpoint

---
+++ The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VillaAPIController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Villa> ObterVillas()
+    {
+        return
+        [
+            new Villa { Id = 1, Nome = "Villa 1" },
+            new Villa { Id = 2, Nome = "Villa 2" },
+            new Villa { Id = 3, Nome = "Villa 3" }
+        ];
+    }
+}
---
+++ The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Models/Villa.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicVilla.API.Models;
+
+public class Villa
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
