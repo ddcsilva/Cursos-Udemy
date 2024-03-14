0faca2949846a24fda4c9e4b0eefeb3d1c1d0744
danilo.silva <danilo.silva@msn.com>
2024-03-13 22:50:57 -0300

    Resposta Personalizada

--- The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Controllers/VilasController.cs
+++ The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Controllers/VilasController.cs
@@ -1,11 +1,10 @@
-﻿using AutoMapper;
-using MagicVilla.API.Logging;
+﻿using System.Net;
+using AutoMapper;
 using MagicVilla.API.Models;
 using MagicVilla.API.Models.DTO;
 using MagicVilla.API.Repository.IRepository;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla.API.Controllers;
 
@@ -14,156 +13,243 @@ namespace MagicVilla.API.Controllers;
 public class VilasController : ControllerBase
 {
     private readonly IVilaRepository _vilaRepository;
-    private readonly ILogging _logger;
     private readonly IMapper _mapper;
+    protected APIResponse _response;
 
-    public VilasController(IVilaRepository vilaRepository, ILogging logger, IMapper mapper)
+    public VilasController(IVilaRepository vilaRepository, IMapper mapper)
     {
         _vilaRepository = vilaRepository;
-        _logger = logger;
         _mapper = mapper;
+        _response = new APIResponse();
     }
 
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<ActionResult<IEnumerable<VilaDTO>>> ObterVilas()
+    public async Task<ActionResult<APIResponse>> ObterVilas()
     {
-        IEnumerable<Vila> vilas = await _vilaRepository.ObterTodos();
+        try
+        {
+            IEnumerable<Vila> vilas = await _vilaRepository.ObterTodos();
 
-        _logger.Log("Obtendo todas as Villas", "info");
-        return Ok(_mapper.Map<IEnumerable<VilaDTO>>(vilas));
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.Resultado = _mapper.Map<IEnumerable<VilaDTO>>(vilas);
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.Sucesso = false;
+            _response.MensagensDeErro = new List<string> { ex.ToString() };
+        }
+
+        return _response;
     }
 
     [HttpGet("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<VilaDTO>> ObterVila(int id)
+    public async Task<ActionResult<APIResponse>> ObterVila(int id)
     {
-        if (id == 0)
+        try
         {
-            _logger.Log("Id inválido", "error");
-            return BadRequest();
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                return BadRequest(_response);
+            }
+
+            var vila = await _vilaRepository.Obter(v => v.Id == id);
+
+            if (vila == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Sucesso = false;
+                return NotFound(_response);
+            }
+
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.Resultado = _mapper.Map<VilaDTO>(vila);
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.Sucesso = false;
+            _response.MensagensDeErro = new List<string> { ex.ToString() };
         }
 
-        var vila = await _vilaRepository.Obter(v => v.Id == id);
-
-        if (vila == null)
-        {
-            _logger.Log($"Vila não encontrada: {id}", "error");
-            return NotFound();
-        }
-
-        _logger.Log($"Obtendo Vila: {id}", "info");
-        return Ok(_mapper.Map<VilaDTO>(vila));
+        return _response;
     }
 
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<VilaDTO>> AdicionarVila([FromBody] CriarVilaDTO criarVilaDTO)
+    public async Task<ActionResult<APIResponse>> AdicionarVila([FromBody] CriarVilaDTO criarVilaDTO)
     {
-        if (criarVilaDTO == null)
+        try
         {
-            _logger.Log("Vila inválida", "error");
-            return BadRequest(criarVilaDTO);
+            if (criarVilaDTO == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                return BadRequest(_response);
+            }
+
+            if (await _vilaRepository.Obter(v => v.Nome == criarVilaDTO.Nome) != null)
+            {
+                ModelState.AddModelError("Nome", "Nome já existe");
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                _response.MensagensDeErro = ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage).ToList();
+                return BadRequest(_response);
+            }
+
+            Vila model = _mapper.Map<Vila>(criarVilaDTO);
+
+            await _vilaRepository.Adicionar(model);
+
+            _response.StatusCode = HttpStatusCode.Created;
+            _response.Resultado = _mapper.Map<VilaDTO>(model);
+
+            return CreatedAtAction(nameof(ObterVila), new { id = model.Id }, _response);
+        }
+        catch (Exception ex)
+        {
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.Sucesso = false;
+            _response.MensagensDeErro = new List<string> { ex.ToString() };
         }
 
-        if (await _vilaRepository.Obter(v => v.Nome == criarVilaDTO.Nome) != null)
-        {
-            ModelState.AddModelError("Nome", "Nome já existe");
-            _logger.Log("Nome já existe", "error");
-            return BadRequest(ModelState);
-        }
-
-        Vila model = _mapper.Map<Vila>(criarVilaDTO);
-
-        await _vilaRepository.Adicionar(model);
-
-        _logger.Log($"Vila adicionada: {model.Id}", "info");
-        return CreatedAtAction(nameof(ObterVila), new { id = model.Id }, model);
+        return _response;
     }
 
     [HttpPut("{id:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult> AtualizarVila(int id, [FromBody] AtualizarVilaDTO atualizarVilaDTO)
+    public async Task<ActionResult<APIResponse>> AtualizarVila(int id, [FromBody] AtualizarVilaDTO atualizarVilaDTO)
     {
-        if (atualizarVilaDTO == null || id != atualizarVilaDTO.Id)
+        try
         {
-            _logger.Log("Vila inválida", "error");
-            return BadRequest();
+            if (atualizarVilaDTO == null || id != atualizarVilaDTO.Id)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                return BadRequest(_response);
+            }
+
+            Vila model = _mapper.Map<Vila>(atualizarVilaDTO);
+
+            await _vilaRepository.Atualizar(model);
+
+            _response.StatusCode = HttpStatusCode.NoContent;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.Sucesso = false;
+            _response.MensagensDeErro = new List<string> { ex.ToString() };
         }
 
-        Vila model = _mapper.Map<Vila>(atualizarVilaDTO);
-
-        await _vilaRepository.Atualizar(model);
-
-        _logger.Log($"Vila atualizada: {id}", "info");
-        return NoContent();
+        return _response;
     }
 
     [HttpDelete("{id:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult> ExcluirVila(int id)
+    public async Task<ActionResult<APIResponse>> ExcluirVila(int id)
     {
-        if (id <= 0)
+        try
         {
-            _logger.Log($"Id inválido: {id}", "error");
-            return BadRequest();
+            if (id <= 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                return BadRequest(_response);
+            }
+
+            var vila = await _vilaRepository.Obter(v => v.Id == id);
+            if (vila == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Sucesso = false;
+                return NotFound(_response);
+            }
+
+            await _vilaRepository.Remover(vila);
+
+            _response.StatusCode = HttpStatusCode.NoContent;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.Sucesso = false;
+            _response.MensagensDeErro = new List<string> { ex.ToString() };
         }
 
-        var vila = await _vilaRepository.Obter(v => v.Id == id);
-        if (vila == null)
-        {
-            _logger.Log($"Vila não encontrada: {id}", "error");
-            return NotFound();
-        }
-
-        await _vilaRepository.Remover(vila);
-
-        _logger.Log($"Vila excluída: {id}", "info");
-        return NoContent();
+        return _response;
     }
 
     [HttpPatch("{id:int}", Name = "AtualizarVilaParcial")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> AtualizarParcialVilla(int id, [FromBody] JsonPatchDocument<AtualizarVilaDTO> patch)
+    public async Task<ActionResult<APIResponse>> AtualizarParcialVilla(int id, [FromBody] JsonPatchDocument<AtualizarVilaDTO> patch)
     {
-        if (patch == null || id == 0)
+        try
         {
-            _logger.Log("Vila inválida", "error");
-            return BadRequest();
+            if (patch == null || id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                return BadRequest(_response);
+            }
+
+            var vila = await _vilaRepository.Obter(v => v.Id == id, false);
+
+            AtualizarVilaDTO vilaDTO = _mapper.Map<AtualizarVilaDTO>(vila);
+
+            if (vila == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Sucesso = false;
+                return NotFound(_response);
+            }
+
+            patch.ApplyTo(vilaDTO, ModelState);
+
+            Vila model = _mapper.Map<Vila>(vilaDTO);
+
+            await _vilaRepository.Atualizar(model);
+
+            if (!ModelState.IsValid)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Sucesso = false;
+                _response.MensagensDeErro = ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage).ToList();
+                return BadRequest(_response);
+            }
+
+            _response.StatusCode = HttpStatusCode.NoContent;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.Sucesso = false;
+            _response.MensagensDeErro = new List<string> { ex.ToString() };
         }
 
-        var vila = await _vilaRepository.Obter(v => v.Id == id, false);
-
-        AtualizarVilaDTO vilaDTO = _mapper.Map<AtualizarVilaDTO>(vila);
-
-        if (vila == null)
-        {
-            _logger.Log($"Vila não encontrada: {id}", "error");
-            return NotFound();
-        }
-
-        patch.ApplyTo(vilaDTO, ModelState);
-
-        Vila model = _mapper.Map<Vila>(vilaDTO);
-
-        await _vilaRepository.Atualizar(model);
-
-        if (!ModelState.IsValid)
-        {
-            _logger.Log("Vila inválida", "error");
-            return BadRequest(ModelState);
-        }
-
-        _logger.Log($"Vila atualizada parcialmente: {id}", "info");
-        return NoContent();
+        return _response;
     }
 }
---
+++ The Complete Guide (.NET7 API) Part 1/MagicVilla/MagicVilla.API/Models/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MagicVilla.API.Models;
+
+public class APIResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool Sucesso { get; set; } = true;
+    public List<string> MensagensDeErro { get; set; }
+    public object Resultado { get; set; }
+}
