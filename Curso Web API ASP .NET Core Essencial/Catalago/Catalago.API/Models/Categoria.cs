ff7d04d2c873a7202e854308c7ed42643299374a
danilo.silva <danilo.silva@msn.com>
2024-04-14 19:42:37 -0300

    Aplicando o Data Annotations

--- Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Models/Categoria.cs
+++ Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Models/Categoria.cs
@@ -1,10 +1,13 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Catalago.Api.Models;
 
 /// <summary>
 /// Classe que representa uma categoria de produtos
 /// </summary>
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
@@ -13,8 +16,15 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(80, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
     public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(300, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
     public string? ImagemUrl { get; set; }
 
     // Relacionamento 1:N (1 Categoria possui N Produtos)
--- Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Models/Produto.cs
+++ Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Models/Produto.cs
@@ -1,16 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Catalago.Api.Models;
 
 /// <summary>
 /// Classe que representa um produto
 /// </summary>
+[Table("Produtos")]
 public class Produto
 {
-    public int ProdutoId { get; set; }
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(80, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
     public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(300, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [Column(TypeName = "decimal(10, 2)")]
     public decimal Preco { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(300, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
     public string? ImagemUrl { get; set; }
+
     public float? Estoque { get; set; }
+
     public DateTime DataCadastro { get; set; }
 
     // Relacionamento N:1 (N Produtos pertencem a 1 Categoria)
