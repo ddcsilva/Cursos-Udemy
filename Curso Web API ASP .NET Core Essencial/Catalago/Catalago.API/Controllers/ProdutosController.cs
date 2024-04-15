732628ee2e53e444e50870506e0d057b59163d46
danilo.silva <danilo.silva@msn.com>
2024-04-15 10:12:20 -0300

    ProdutosController - GET

---
+++ Curso Web API ASP .NET Core Essencial/Catalago/Catalago.API/Controllers/ProdutosController.cs
@@ -0,0 +1,33 @@
+﻿using Catalago.Api.Context;
+using Catalago.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalago.API.Controllers;
+
+/// <summary>
+/// Controller responsável por gerenciar as operações relacionadas a produtos
+/// </summary>
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly CatalogoContext _context;
+
+    public ProdutosController(CatalogoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+
+        if (produtos == null)
+        {
+            return NotFound("Nenhum produto encontrado");
+        }
+
+        return produtos;
+    }
+}
